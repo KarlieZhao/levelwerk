39031c1f834088849c5dbb8ccedde4ea92026749
Karlie Zhao <62089883+KarlieZhao@users.noreply.github.com>
2024-12-25 21:54:23 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -5,35 +5,38 @@ import { ScrollControls, Sky, useScroll, useGLTF, useAnimations } from '@react-t
 
 export default function App() {
   return (
-    <Canvas shadows camera={{ position: [0, 0, 10] }}>
-      <ambientLight intensity={0.03} />
-      <fog attach="fog" args={['#ff5020', 5, 18]} />
+   <div className='landing-main'>
+    <Canvas shadows camera={{ position: [0, 0, 10] }}  gl={{ setClearColor: ['#000000', 1] }}>
+      <ambientLight intensity={0.1} />
+      <fog attach="fog" args={['#ff5020', 7, 18]} />
+      {/* ff5020 */}
       <spotLight angle={0.14} color="#ffd0d0" penumbra={1} position={[25, 50, -20]} shadow-mapSize={[2048, 2048]} shadow-bias={-0.0001} castShadow />
-      <Sky scale={1000} sunPosition={[2, 0.4, 10]} />
+      {/* <Sky scale={1000} sunPosition={[2, 0.4, 10]} /> */}
       <Suspense fallback={null}>
         {/* Wrap contents you want to scroll into <ScrollControls> */}
         <ScrollControls pages={3}>
-          <LittlestTokyo scale={0.02} position={[0, 2.5, 0]} />
+          <LittlestTokyo scale={0.02} position={[2, 1.5,-1]} />
         </ScrollControls>
       </Suspense>
     </Canvas>
+
+   <div className='title'>LevelWerk</div> 
+    </div> 
   )
 }
 
 function LittlestTokyo({ ...props }) {
   // This hook gives you offets, ranges and other useful things
   const scroll = useScroll()
-  const { scene, nodes, animations } = useGLTF('/LittlestTokyo-transformed.glb')
-  const { actions } = useAnimations(animations, scene)
+  const { scene, nodes } = useGLTF('/room_big3.glb')
   useLayoutEffect(() => Object.values(nodes).forEach((node) => (node.receiveShadow = node.castShadow = true)))
-  useEffect(() => void (actions['Take 001'].play().paused = true), [actions])
   useFrame((state, delta) => {
-    const action = actions['Take 001']
-    // The offset is between 0 and 1, you can apply it to your models any way you like
     const offset = 1 - scroll.offset
-    action.time = THREE.MathUtils.damp(action.time, (action.getClip().duration / 2) * offset, 100, delta)
-    state.camera.position.set(Math.sin(offset) * -10, Math.atan(offset * Math.PI * 2) * 5, Math.cos((offset * Math.PI) / 3) * -10)
-    state.camera.lookAt(0, 0, 0)
+    state.camera.position.set(
+    Math.sin(offset) * -10, 
+    Math.atan(offset * Math.PI * 2) * 5,
+    Math.cos((offset * Math.PI) / 3) * -4)
+    state.camera.lookAt(0.3, 0, 0)
   })
   return <primitive object={scene} {...props} />
 }
@@ -43,4 +46,4 @@ author: glenatron (https://sketchfab.com/glenatron)
 license: CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
 source: https://sketchfab.com/models/94b24a60dc1b48248de50bf087c0f042
 title: Littlest Tokyo */
-useGLTF.preload('/LittlestTokyo-transformed.glb')
+useGLTF.preload('/room_big3.glb')
