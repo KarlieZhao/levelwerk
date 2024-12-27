a38edca956fbdae755b5ae01f4c08d89585e17f0
Karlie Zhao <62089883+KarlieZhao@users.noreply.github.com>
2024-12-27 17:38:39 -0500

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -5,9 +5,22 @@ export default function About() {
         <NavBar showIcon={true}/>
 <main className="about">
 <div>
-<p>LevelWerk is an artist-run alternative art space in Brooklyn, NY, committed to supporting both local and international artists while building community through its residency program.</p>
-<p>It offers a flexible, open environment where resident artists are encouraged to connect with the local community, reflect on their practices, experiment, host workshops, and explore the boundless possibilities of New Media.
-</p>
+<p>LevelWerk is an artist-run alternative art space in Brooklyn, NY, committed to supporting both local and international artists while building community through its residency program.
+It offers a flexible, open environment where resident artists are encouraged to connect with the local community, reflect on their practices, experiment, host workshops, and explore the boundless possibilities of New Media.
+</p><br/>
+<p>Here's what we can offer: 
+    <ul>
+    <li>A full-size sofa bed</li>
+    <li>Social media promotion</li>
+        <li>Interview & Media coverage opportunites</li>
+        <li>Online or small-scale on-site exhibitions</li>
+        <li>Private workspace with Wi-Fi</li>
+        <li>Full kitchen and occasional free meals</li>
+        <li>Tools: a 3D printer, installation/sculpture toolkit, soldering station, projector, drop cloth, etc.</li>
+        <li>Studio visits</li>
+        <li>A recommendation list of our favorite coffee spots and galleries!</li>
+    </ul>
+</p><br/>
 <p>Past residents info coming soon!</p>
 </div>
 </main>
