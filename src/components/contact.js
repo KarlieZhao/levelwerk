3776d646b0a8021d94a074a28256845140a42191
Karlie Zhao <62089883+KarlieZhao@users.noreply.github.com>
2024-12-27 17:45:35 -0500

    update contacts

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,9 +6,13 @@ export default function Contact(){
         <div>
         <NavBar showIcon={true}/>
         <main className="contact">
-            <div>
-                <p>Email: levelwerk.ny@gmail.com</p>
-                <p>Instagram: @levelwerkny</p>
+        
+        <div >
+            <h2>Wants to collab with us? </h2>
+            <p>We're always on the lookout for artists seeking a place to crash in Brooklyn, wanting to run a workshop, having an exhibition idea, or just like to chat about art. 
+            </p><br/>
+                <h3>Email: levelwerk.ny@gmail.com</h3>
+                <h3>Instagram: @levelwerkny</h3>
                 </div>
           </main>
         </div>
