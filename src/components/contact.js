d13cd8247c548b0d9665ff561e06865c3314bfc8
Karlie Zhao <62089883+KarlieZhao@users.noreply.github.com>
2025-01-15 14:00:06 -0500

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,8 +8,8 @@ export default function Contact(){
         <main className="contact">
         
         <div >
-            <h2>Want to collab with us? </h2>
-            <p>We're always on the lookout for artists seeking a place to crash in Brooklyn, wanting to run a workshop, having an exhibition idea, or just like to chat about art. 
+            <h2>Come say hi! </h2>
+            <p>We're always on the lookout for artists who are seeking a place to crash, have an exhibition/workshop idea, or just want to chat about art. 
             </p><br/>
                 <h3>Email: levelwerk.ny@gmail.com</h3>
                 <h3>Instagram: @levelwerkny</h3>
